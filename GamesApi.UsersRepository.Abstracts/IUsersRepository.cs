8a106c1bae1fce56b777330cefc24659bed8df2c
mchangus <mchangus78@gmail.com>
2024-02-12 08:30:51 -0500

    class in wrong location

---
+++ GamesApi.UsersRepository.Abstracts/IUsersRepository.cs
@@ -0,0 +1,29 @@
+﻿using Games.Core.Models;
+
+namespace GamesApi.UsersRepository.Abstracts
+{
+    /// <summary>
+    /// The User repository interface
+    /// </summary>
+    public interface IUsersRepository
+    {
+        /// <summary>
+        /// Create a new user
+        /// </summary>
+        /// <returns><see cref="User"/></returns>
+        User Create();
+
+        /// <summary>
+        /// Get an user given its id
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns><see cref="User"/></returns>
+        User? GetById(int userId);
+
+        /// <summary>
+        /// Update user
+        /// </summary>
+        /// <param name="user"></param>
+        void UpdateUser(User user);
+    }
+}
